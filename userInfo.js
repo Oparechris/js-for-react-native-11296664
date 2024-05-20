ea1f80dbf7348241f708e52f503d66e1f6ae43d0
Oparechris <richiechris355@gmail.com>
2024-05-19 22:28:03 -0400

    task 3

---
+++ userInfo.js
@@ -0,0 +1,14 @@
+//task 3
+function createUserProfiles(originalNames, modifiedNames) {
+    if (originalNames.length !== modifiedNames.length) {
+        throw new Error("Both arrays must have the same length");
+    }
+    
+    return originalNames.map((name, index) => {
+        return {
+            id: index + 1,
+            originalName: name,
+            modifiedName: modifiedNames[index]
+        };
+    });
+}
